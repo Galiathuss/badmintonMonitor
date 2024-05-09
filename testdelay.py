e42450964173a9bc9775420e62d0b436328544db
McD 1s Yummy <37834529+Galiathuss@users.noreply.github.com>
2024-05-09 16:06:05 +0800

    Update testdelay.py

--- testdelay.py
+++ testdelay.py
@@ -4,8 +4,8 @@ import time
 
 async def send_post_request():
     # 定义你的 FastAPI 服务的 URL
-    # url = "http://104.219.236.45:8133/testdelay"
-    url = "http://127.0.0.1:8133/testdelay"
+    url = "http://104.219.236.45:8133/testdelay"
+    # url = "http://127.0.0.1:8133/testdelay"
 
     # 创建要发送的数据
     data = {"ts": time.time() * 1000}
@@ -15,7 +15,6 @@ async def send_post_request():
         # 发送 POST 请求
         async with session.post(url, json=data) as response:
             # 等待响应并打印结果
-            print("Status Code:", response.status)
             response_data = await response.json()
             print("Response Body:", response_data)
 
